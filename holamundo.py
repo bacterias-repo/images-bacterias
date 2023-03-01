e68e575c0e49ca738be255229b52cdd214f28c8b
Arturo Jimenez <artdan2410@gmail.com>
2023-03-01 13:24:19 -0600

    subir el hola mundo

---
+++ holamundo.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  1 23:46:00 2023
+
+@author: artur
+"""
+
+import matplotlib
+import matplotlib.pyplot as plt 
+from matplotlib.patches import Rectangle
+import csv
+import numpy as np
+import random as rn
+import math as mt
+import glob
+import pandas as pd
+#import cv2
+import matplotlib.patches as patches
+from os import remove
+
+from pandas import DataFrame
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+print("hola mundo!")
