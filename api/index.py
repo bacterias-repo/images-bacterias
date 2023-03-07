3c37cafa70bf9e45855ea1953d3514840db7a348
Arturo Jimenez <artdan2410@gmail.com>
2023-03-06 19:01:29 -0600

    prueba 1

---
+++ api/index.py
@@ -0,0 +1,20 @@
+from http.server import BaseHTTPRequestHandler
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+class handler(BaseHTTPRequestHandler):
+
+def window():
+   app = QApplication(sys.argv)
+   w = QWidget()
+   b = QLabel(w)
+   b.setText("Hello World!")
+   w.setGeometry(100,100,200,50)
+   b.move(50,20)
+   w.setWindowTitle("PyQt5")
+   w.show()
+   sys.exit(app.exec_())
+if __name__ == '__main__':
+   window()
