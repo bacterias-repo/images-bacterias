36584bb0bc32161f50bc9507f83072e0c444eb7e
bacterias-repo <126705858+bacterias-repo@users.noreply.github.com>
2023-04-04 14:30:27 -0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,18 +1,12 @@
 import cv2
 import numpy as np
-import urllib.request
-from io import BytesIO
-from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
-@app.post("/convert_grayscale")
-async def convert_grayscale(url: str):
-    with urllib.request.urlopen(url) as url:
-        img_array = np.asarray(bytearray(url.read()), dtype=np.uint8)
-        img = cv2.imdecode(img_array, cv2.IMREAD_GRAYSCALE)
-        buf = BytesIO()
-        cv2.imwrite(buf, img, format='png')
-        buf.seek(0)
-        return StreamingResponse(buf, media_type="image/png")
+@app.post("/convert_image")
+async def convert_image(file: UploadFile = File(...)):
+    file_contents = await file.read()
+    nparr = np.fromstring(file_contents, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
+    return {"image": img.tolist()}
