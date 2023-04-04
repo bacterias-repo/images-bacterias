e60202b2b9a83cc32f2271eb70da6887ad773826
bacterias-repo <126705858+bacterias-repo@users.noreply.github.com>
2023-04-04 14:40:31 -0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,15 @@
 import cv2
 import numpy as np
 from fastapi import FastAPI, File, UploadFile
+from starlette.responses import StreamingResponse
 
 app = FastAPI()
 
-@app.post("/convert_image")
-async def convert_image(file: UploadFile = File(...)):
-    file_contents = await file.read()
-    nparr = np.fromstring(file_contents, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
-    return {"image": img.tolist()}
+@app.post("/grayscale")
+async def grayscale(file: UploadFile):
+    contents = await file.read()
+    np_image = np.fromstring(contents, np.uint8)
+    image = cv2.imdecode(np_image, cv2.IMREAD_COLOR)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    buffer = cv2.imencode('.jpg', gray_image)[1].tobytes()
+    return StreamingResponse(BytesIO(buffer), media_type="image/jpeg")
