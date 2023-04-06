96c2a6485ee65ac597eb87d9d4a76a76c5e97f46
bacterias-repo <126705858+bacterias-repo@users.noreply.github.com>
2023-04-05 23:31:13 -0600

    Update and rename main.py to api.py

---
+++ api.py
@@ -0,0 +1,27 @@
+import os
+import cv2
+import numpy as np
+import requests
+from github import Github
+
+def convert_to_grayscale(image_path):
+    image = cv2.imread(image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return gray
+
+def save_to_github(image_path, github_token):
+    g = Github(github_token)
+    repo = g.get_repo("username/repo-name")
+    with open(image_path, "rb") as f:
+        content = f.read()
+    repo.create_file(f"images/{image_path}", f"Upload {image_path}", content)
+
+def main():
+    image_path = "images/image.jpg"
+    gray = convert_to_grayscale(image_path)
+    cv2.imwrite(image_path, gray)
+    github_token = os.environ["GITHUB_TOKEN"]
+    save_to_github(image_path, github_token)
+
+if __name__ == '__main__':
+    main()
